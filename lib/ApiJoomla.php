4ba17d1ba34b860f5a7cdd03cb189b2ea37c8a50
Romans Malinovskis <r@agiletech.ie>
2012-09-09 15:00:28 +0100

    .

--- lib/ApiJoomla.php
+++ lib/ApiJoomla.php
@@ -3,6 +3,38 @@
 	Implements support for Joomla
 	http://agiletoolkit.org/a/joomla
 */
-class ApiJoomla extends ApiWeb {
+class ApiJoomla extends ApiFrontend {
+	/* 
+		Specify NULL as argument to use default connectivity method. You may
+		also specify a custom DSN for connectivity
 
+		Use config.pdo_dsn to override connection string for Aglie Toolkit.
+
+		http://agiletoolkit.org/a/joomla/db
+	*/
+
+	function dbConnect($dsn=undefined){
+		if($dsn !== undefined)return parent::dbConnect($dsn);
+
+		$conf =& JFactory::getConfig();
+
+		$pdo_dsn 	= $conf->getValue('config.pdo_dsn');
+		if($pdo_dsn)return parent::dbConnect($pdo_dsn);
+
+
+		$host 		= $conf->getValue('config.host');
+		$user 		= $conf->getValue('config.user');
+		$password 	= $conf->getValue('config.password');
+		$database	= $conf->getValue('config.db');
+		$prefix 	= $conf->getValue('config.dbprefix');
+		$driver 	= $conf->getValue('config.dbtype');
+		$debug 		= $conf->getValue('config.debug');
+
+		$pdo_dsn=array("$driver:host=$host;dbname=$database",
+			$user,
+			$password);
+
+		return parent::dbConnect($pdo_dsn);
+
+	}
 }
---
+++ lib/JoomlaPathFinder.php
@@ -0,0 +1,4 @@
+<?php
+class JoomlaPathFinder extends PathFinder {
+	
+}
